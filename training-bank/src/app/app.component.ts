ee990f9f0fd4d55b1d0311d402863ab541775519
Gabriel Roland Carvalho Fonseca <GRCFO@bndes.net>
2019-12-02 13:49:09 -0300

    Adicionado metodos

--- training-bank/src/app/app.component.ts
+++ training-bank/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'training-bank';
+
+  constructor(public dialog: MatDialog){}
+    
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog, {
+      height: '350px'
+    });
+  dialogRef.afterClosed().subscribe(result => {console.log(`Dialog result: ${result}`);
+  });
+  
 }
--- training-bank/src/app/app.module.ts
+++ training-bank/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatDialogModule} from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -10,7 +11,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
