220a5e10923247f3857a1ad58e5c391fd4694ef7
Gabriel Roland Carvalho Fonseca <GRCFO@bndes.net>
2019-12-03 11:28:12 -0300

    Adicionado conteúdo ao content component.ts

--- training-bank/src/app/content/content.component.ts
+++ training-bank/src/app/content/content.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as Typed from 'typed.js/lib/typed.js'
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -10,6 +10,19 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const options = {
+      stringsElement: '#typed-strings',
+      strings: ['Training banking juros amigo', 'Peça a portabilidade e venha ser Training Bankinhg hoje mesmo', 'Inovavor digital e seguro'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
   }
-
+  typed = new Typed('.typing-element', options);
 }
+ 
